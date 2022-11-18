f42fdfc87fb8ac0d30c2481ad9afee32176c1241
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-11-18 13:38:03 -0600

    Added detail code for Queue using Stack

--- QueueUsingStack.py
+++ QueueUsingStack.py
@@ -4,20 +4,56 @@ input:[2,3,7,8]
 output: pop() --> 2
 """
 
-list1, list2 = [],[]
+class QueueUsingStack:
 
-def Enqueue(num):
-    list1.append(num)
+    def __init__(self):
+        self.list1 =[]
+        self.list2 = []
+    
+    def push(self,num):
+        self.list1.append(num)
+        print(self.list1)
+        return "Item Added"
+    
+    def pop(self):
+        if len(self.list2) != 0:
+            return self.list2.pop()
+        if len(self.list2) == 0 and len(self.list1) > 0:
+            while len(self.list1) > 0:
+                self.list2.append(self.list1.pop())
+            return self.list2.pop()
+        else:
+            return "No items to remove"
+            
+
+    def peek(self):
+        if len(self.list2) != 0:
+            return self.list2[-1]
+        if len(self.list2) == 0 and len(self.list1) > 0:
+            while len(self.list1) > 0:
+                self.list2.append(self.list1.pop())
+            return self.list2[-1]
+        else:
+            return "No items to view"
+    
+    def empty(self):
+        if len(self.list1) == 0 and len(self.list2) == 0:
+            return True
+        else:
+            return False
 
 
-def Dequeue():
-    if len(list2) == 0:
-        while len(list1) > 0:
-            list2.append(list1.pop())
-    list2.pop()
-    print(list2)
+queue = QueueUsingStack()
+print(queue.push(3))
+print(queue.push(5))
+print(queue.push(7))
+
+print(queue.pop())
+print(queue.peek())
+print(queue.pop())
+print(queue.empty())
+print(queue.pop())
+print(queue.empty())
+print(queue.pop())
 
 
-Enqueue(2)
-Enqueue(3)
-Dequeue()
